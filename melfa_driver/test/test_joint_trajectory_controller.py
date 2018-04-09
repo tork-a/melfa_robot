ff1a31ac0de897183bfaedc45934f976104b5d5d
Ryosuke Tajima <ryosuke.tajima@opensource-robotics.tokyo.jp>
2018-04-09 15:46:43 +0900

    Add first test

---
+++ melfa_driver/test/test_joint_trajectory_controller.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import math
+import sys
+import actionlib
+from control_msgs.msg import (
+    FollowJointTrajectoryAction,
+    FollowJointTrajectoryActionGoal)
+import rospy
+import rostest
+from sensor_msgs.msg import JointState
+from trajectory_msgs.msg import JointTrajectory
+import time
+import unittest
+
+
+class TestJointTrajectoryController(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        rospy.init_node('test_joint_trajectory_controller_node')
+
+    def setUp(self):
+        self.joint_states_list = []
+
+        rospy.Subscriber('joint_states',
+                         JointState, self.cb_joint_states, queue_size=10)
+        
+        self.pub = rospy.Publisher(
+            'joint_trajectory_controller/command',
+            JointTrajectory,
+            queue_size=10)
+        
+        self.client = actionlib.SimpleActionClient(
+            'joint_trajectory_controller/follow_joint_trajectory',
+            FollowJointTrajectoryAction)
+        self.client.wait_for_server()
+        
+    def cb_joint_states(self, msg):
+        self.joint_states_list.append(msg)
+        
+    def test_joint_states(self):
+        time.sleep(1.0)
+        self.assertTrue(self.joint_states_list)
+        
+if __name__ == '__main__':
+    rostest.rosrun('melfa_driver',
+                   'test_joint_trajectory_controller',
+                   TestJointTrajectoryController)
